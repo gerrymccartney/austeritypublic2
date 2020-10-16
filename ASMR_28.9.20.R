58f7ef105f045cdf9476871fe28b27a3789e98cb
Gerry McCartney <gmccartney@nhs.net>
2020-10-16 08:27:52 +0100

    Updated ASMR calculation and facet graphs. Problem that the lines aren't continuous despite no missing data. Need to discuss with Frank.

--- ASMR_28.9.20.R
+++ ASMR_28.9.20.R
@@ -10,7 +10,11 @@ names(list_of_files) <- list_of_files
 df <- list_of_files %>%
   map_dfr(~read_table(.x,
                       skip = 2, na = "."),.id="File")
-df<-(as_tibble(df))
+df<-(as_tibble(df)) 
+
+#code to change NAs to 0
+
+df[is.na(df)] <- 0
 
 #working directory changed back
 
@@ -24,21 +28,77 @@ Standard <- read_excel("D:/Mortality_trends/Austerity/Austerity_analysis/austeri
 
 #fixes standard population as numeric
 
-Standard$age <- as.character(Standard$Age)
+Standard$Age <- as.character(Standard$Age)
 Standard$Popn <- as.numeric(Standard$Popn)
 
 #joining standard population and mortality data to create df2, matching on age group 
 
-df2 <- merge(Standard, df, by = "Age")
+df2 <- merge(df, Standard, by = "Age")
+
+#merge in more useful country labels
+
+Countries <- read_excel("D:/Mortality_trends/Austerity/Austerity_analysis/austeritypublic2/Country_list.xlsx")
+df3 <- merge(df2, Countries, by = "File")
+
 
 #calculation of ASMRs for each country and year
+#residual problems with missing data and useful labels not being carried over in merge
+##START HERE
 
-asmr <- df2 %>%
+asmr <- df3 %>%
   group_by(File, Year) %>%
-  summarise(asmrf = sum(Female*Popn, na.rm = TRUE), 
-            asmrm = sum(Male*Popn, na.rm = TRUE), 
-            asmrt = sum(Total*Popn, na.rm = TRUE))
+  summarise(asmrF = sum(Female*Popn, na.rm = TRUE), 
+            asmrM = sum(Male*Popn, na.rm = TRUE), 
+            asmrT = sum(Total*Popn, na.rm = TRUE))
 
 
+#start plotting - intention is to do 'small multiples' using 'facet_wrap()'
+#https://www.r-graph-gallery.com/223-faceting-with-ggplot2.html 
 
+#library(tidyverse)
+library(dplyr) 
+library(ggplot2)
+
+#Building code for plot of ASMR for total population
+#Lines are intermittent but there isn't corresponding missing data in the file. ? what is the problem
+
+asmr_plotT <- asmrT %>% 
+  ggplot(aes(x = Year, y = asmr)) +
+  geom_line() + 
+  xlab("Year") +
+  ylab("ASMR") +
+  xlim(1988, 2018) +
+  ylim(0, 2000) +
+  ggtitle("Trend in Age Standardised Mortality Rate (ASMR) for the total population") + 
+  facet_wrap(~File)
+
+asmr_plotT
+
+#Female graph
+
+asmr_plotF <- asmrF %>% 
+  ggplot(aes(x = Year, y = asmr)) +
+  geom_line() + 
+  xlab("Year") +
+  ylab("ASMR") +
+  xlim(1988, 2018) +
+  ylim(0, 2000) +
+  ggtitle("Trend in female Age Standardised Mortality Rate (ASMR)") + 
+  facet_wrap(~File)
+
+asmr_plotF
+
+#Male graph
+
+asmr_plotM <- asmrM %>% 
+  ggplot(aes(x = Year, y = asmr)) +
+  geom_line() + 
+  xlab("Year") +
+  ylab("ASMR") +
+  xlim(1988, 2018) +
+  ylim(0, 2000) +
+  ggtitle("Trend in male Age Standardised Mortality Rate (ASMR)") + 
+  facet_wrap(~File)
+
+asmr_plotM
 
